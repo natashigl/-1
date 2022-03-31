030dccd1ab35fe3c252d2c8dec6f506f31fa4278
Наталья Гладышева <natalyagl18@gmail.com>
2022-03-31 17:07:00 +0300

    Урок 02 - Создание точки

--- практика1/практика1.cpp
+++ практика1/практика1.cpp
@@ -1,10 +1,21 @@
-﻿#include <iostream>
+﻿#include <GL/glew.h>
+#include <iostream>
 #include <GL/freeglut.h>
 #include <GL/glut.h>
+#include "math_3d.h"
+
+GLuint VBO;
 
 static void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_POINTS, 0, 1);
+	glDisableVertexAttribArray(0);
+
 	glutSwapBuffers();
 }
 
@@ -19,6 +30,25 @@ int main(int argc, char** argv)
 
 	glutDisplayFunc(RenderSceneCB);
 
+	/*glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glutMainLoop();*/
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	Vector3f Vertices[1];
+	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
 	glutMainLoop();
+
 }
